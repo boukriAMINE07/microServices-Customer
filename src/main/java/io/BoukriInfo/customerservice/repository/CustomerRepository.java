4c918d088303b1d07452876ca83936aba4556fb1
Boukri Amine <AMINE_BOUKRI@emsi-edu.ma>
2022-12-16 11:47:52 +0100

    Customer Service Microservice

---
+++ src/main/java/io/BoukriInfo/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package io.BoukriInfo.customerservice.repository;
+
+import io.BoukriInfo.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
+
+}
